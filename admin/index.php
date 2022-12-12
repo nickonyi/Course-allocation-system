19055313f2d653672a62b6a116faf04998367d85
nickonyi <nickoluoch1130@gmail.com>
2022-12-13 00:01:34 +0300

    course allocation system

--- admin/index.php
+++ admin/index.php
@@ -9,8 +9,8 @@ session_start();
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./css/style.css">
-    <script defer src="./scripts/script.js"></script>
+    <link rel="stylesheet" href="../css/style.css">
+    <script defer src="../scripts/script.js"></script>
     <title>Course allocation system</title>
 </head>
 
@@ -23,29 +23,29 @@ session_start();
     <div class="side-bar">
         <div class="side-bar-header">
             <div>
-                <img src="./assets/icons/icons8-dashboard-layout-48.png" alt="">
+                <img src="../assets/icons/icons8-dashboard-layout-48.png" alt="">
             </div>
             <div class="text">T.U.K admin</div>
         </div>
         <div class="side-bar-contents">
             <div>
-                <img src="./assets/icons/dashboard-solid-48.png" alt="">
+                <img src="../assets/icons/dashboard-solid-48.png" alt="">
             </div>
             <div class="text">Dashboard</div>
             <div>
-                <img src="./assets/icons/form.svg" alt="">
+                <img src="../assets/icons/form.svg" alt="">
             </div>
             <div class="text">Forms</div>
             <div>
-                <img src="./assets/icons/pin.svg" alt="">
+                <img src="../assets/icons/pin.svg" alt="">
             </div>
             <div class="text">Application statistics</div>
             <div>
-                <img src="./assets/icons/stats.svg" alt="">
+                <img src="../assets/icons/stats.svg" alt="">
             </div>
             <div class="text">Student statistics</div>
             <div>
-                <img src="./assets/icons/stats.svg" alt="">
+                <img src="../assets/icons/stats.svg" alt="">
             </div>
             <div class="text">Lecturer statistics</div>
         </div>
@@ -55,23 +55,28 @@ session_start();
     <div class="grid main-content">
         <div class="box content-1">
             <div>
-                <img src="./assets/icons/home.svg" alt="">
+                <img src="../assets/icons/home.svg" alt="">
             </div>
+            <a href="dashboard.php">
             <div class="text">
                 Dashboard
             </div>
+            </a>
+            
         </div>
         <div class="box content-2">
             <div>
-                <img src="./assets/icons/Vector.svg" alt="">
+                <img src="../assets/icons/Vector.svg" alt="">
             </div>
+            <a href="lecturer.php">
             <div class="text">
                 Lecturers
             </div>
+            </a>
         </div>
         <div class="box content-3">
             <div>
-                <img src="./assets/icons/applic.png" alt="">
+                <img src="../assets/icons/applic.png" alt="">
             </div>
             <div class="text">
                 Register
@@ -79,15 +84,18 @@ session_start();
         </div>
         <div class="box content-4">
             <div>
-                <img src="./assets/icons/depart.svg" alt="">
+                <img src="../assets/icons/depart.svg" alt="">
             </div>
+            <a href="department-setup.php">
             <div class="text">
                 Department/Faculties
             </div>
+            </a>
+           
         </div>
         <div class="box content-5">
             <div>
-                <img src="./assets/icons/Vector.svg" alt="">
+                <img src="../assets/icons/Vector.svg" alt="">
             </div>
             <div class="text">
                 Students
@@ -95,7 +103,7 @@ session_start();
         </div>
         <div class="box content-6">
             <div>
-                <img src="./assets/icons/stats.svg" alt="">
+                <img src="../assets/icons/stats.svg" alt="">
             </div>
             <div class="text">
                 Statistics
@@ -103,7 +111,7 @@ session_start();
         </div>
         <div class="box content-7">
             <div>
-                <img src="./assets/icons/form.svg" alt="">
+                <img src="../assets/icons/form.svg" alt="">
             </div>
             <div class="text">
                 Courses
@@ -111,7 +119,7 @@ session_start();
         </div>
         <div class="box content-8">
             <div>
-                <img src="./assets/icons/applic.png" alt="">
+                <img src="../assets/icons/applic.png" alt="">
             </div>
             <div class="text">
                 Applications
